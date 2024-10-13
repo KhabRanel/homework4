4c7f57a4fbc52ebc7bfd368dd4d3fc3958d01cfd
KhabRanel <habibullin.ran@gmail.com>
2024-10-13 20:44:54 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+complete_hw = 12
+hours = 1.5
+course_name = "Python"
+time_for_task = hours / complete_hw
+print(f"Курс: {course_name}, "
+      f"всего задач: {complete_hw}, "
+      f"затрачено часов: {hours}, "
+      f"среднее время выполнения: {time_for_task} часа.")
